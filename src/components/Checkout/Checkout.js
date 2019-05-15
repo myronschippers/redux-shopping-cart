10e0c6d2428058dfd5806933bbb87b298bd248f0
Myron <dubtwee@gmail.com>
2019-05-14 21:14:23 -0500

    using the checkoutReducer to store any item that the user clicks the Add to Cart button next to the individual item

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import mapReduxStateToProps from '../../modules/mapReduxStateToProps';
 
 class Checkout extends Component {
 
     handleCheckout = () => {
         // TODO: Clear the cart and navigate to the product page
-
     }
 
     render() {
+        const htmlCheckoutItems = this.props.reduxState.checkoutReducer.map((item, index) => {
+            return <li key={index}>{item.name}: {item.price}</li>
+        });
+
         return (
             <div>
                 <h2>Checkout</h2>
                 {/* TODO: Display items in the cart */}
-
+                <ul>
+                    {htmlCheckoutItems}
+                </ul>
                 <button onClick={this.handleCheckout}>Checkout</button>
             </div>
         )
     }
 }
 
-export default Checkout;
+export default connect(mapReduxStateToProps)(Checkout);
